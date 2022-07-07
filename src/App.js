bfd7d300897b86cec8f3509574aa39b79591e50a
Alexandru Stanica <alexandru.stanica@teads.tv>
2022-07-07 14:36:51 +0300

    React Component Composition

--- src/App.js
+++ src/App.js
@@ -46,10 +46,11 @@ const App = () => {
 
       <InputWithLabel
         id="search"
-        label="Search"
         value={searchTerm}
         onInputChange={handleSearch}
-      />
+      >
+        <strong>Search:</strong>
+      </InputWithLabel>
 
       <hr />
 
@@ -58,10 +59,16 @@ const App = () => {
   );
 };
 
-const InputWithLabel = ({ id, label, value, type = "text", onInputChange }) => {
+const InputWithLabel = ({
+  id,
+  value,
+  type = "text",
+  onInputChange,
+  children,
+}) => {
   return (
     <>
-      <label htmlFor={id}>{label}</label>
+      <label htmlFor={id}>{children}</label>
       &nbsp;
       <input id={id} type={type} value={value} onChange={onInputChange} />
     </>
