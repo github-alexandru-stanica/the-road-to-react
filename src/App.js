b88dd2c37d528db855c7da2602205085e7ae738a
Alexandru Stanica <alexandru.stanica@teads.tv>
2022-07-12 16:14:20 +0300

    Third-Party Libraries in React

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState, useReducer, useCallback, useEffect, useRef } from "react";
+import axios from "axios";
 
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = useState(localStorage.getItem(key) || initialState);
@@ -59,12 +60,12 @@ const App = () => {
   const handleFetchStories = useCallback(() => {
     dispatchStories({ type: "STORIES_FETCH_INIT" });
 
-    fetch(url)
-      .then((response) => response.json())
+    axios
+      .get(url)
       .then((result) => {
         dispatchStories({
           type: "STORIES_FETCH_SUCCESS",
-          payload: result.hits,
+          payload: result.data.hits,
         });
       })
       .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
