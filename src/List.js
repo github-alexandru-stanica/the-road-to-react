ef7a768f4c4a72d70bd277f0c57a2f2a5e5a3065
Alexandru Stanica <alexandru.stanica@teads.tv>
2022-07-20 12:36:23 +0300

    Sorting

--- src/List.js
+++ src/List.js
@@ -1,21 +1,100 @@
-const List = ({ list, onRemoveItem }) => (
-  <ul>
-    {list.map((item) => (
-      <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
-    ))}
-  </ul>
-);
+import { useState } from "react";
+import { sortBy } from "lodash";
+import styles from "./List.module.css";
+
+const SORTS = {
+  NONE: (list) => list,
+  TITLE: (list) => sortBy(list, "title"),
+  AUTHOR: (list) => sortBy(list, "author"),
+  COMMENT: (list) => sortBy(list, "num_comments").reverse(),
+  POINT: (list) => sortBy(list, "points").reverse(),
+};
+
+const List = ({ list, onRemoveItem }) => {
+  const [sort, setSort] = useState("NONE");
+
+  const handleSort = (sortKey) => {
+    setSort(sortKey);
+  };
+
+  const sortFunction = SORTS[sort];
+  const sortedList = sortFunction(list);
+
+  return (
+    <div>
+      <div style={{ display: "flex" }}>
+        <span style={{ width: "40%" }}>
+          <button
+            type="button"
+            className={
+              sort === "TITLE"
+                ? `${styles.button} ${styles.active}`
+                : styles.button
+            }
+            onClick={() => handleSort("TITLE")}
+          >
+            Title
+          </button>
+        </span>
+        <span style={{ width: "30%" }}>
+          <button
+            type="button"
+            className={
+              sort === "AUTHOR"
+                ? `${styles.button} ${styles.active}`
+                : styles.button
+            }
+            onClick={() => handleSort("AUTHOR")}
+          >
+            Author
+          </button>
+        </span>
+        <span style={{ width: "10%" }}>
+          <button
+            type="button"
+            className={
+              sort === "COMMENT"
+                ? `${styles.button} ${styles.active}`
+                : styles.button
+            }
+            onClick={() => handleSort("COMMENT")}
+          >
+            Comments
+          </button>
+        </span>
+        <span style={{ width: "10%" }}>
+          <button
+            type="button"
+            className={
+              sort === "POINT"
+                ? `${styles.button} ${styles.active}`
+                : styles.button
+            }
+            onClick={() => handleSort("POINT")}
+          >
+            Points
+          </button>
+        </span>
+        <span style={{ width: "10%" }}>Actions</span>
+      </div>
+
+      {sortedList.map((item) => (
+        <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+      ))}
+    </div>
+  );
+};
 
 const Item = ({ item, onRemoveItem }) => {
   return (
-    <li>
-      <span>
+    <li style={{ display: "flex" }}>
+      <span style={{ width: "40%" }}>
         <a href={item.url}>{item.title}</a>
       </span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
-      <span>
+      <span style={{ width: "30%" }}>{item.author}</span>
+      <span style={{ width: "10%" }}>{item.num_comments}</span>
+      <span style={{ width: "10%" }}>{item.points}</span>
+      <span style={{ width: "10%" }}>
         <button type="button" onClick={() => onRemoveItem(item)}>
           Dismiss
         </button>
